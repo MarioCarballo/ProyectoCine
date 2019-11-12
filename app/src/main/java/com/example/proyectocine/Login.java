659f0cff1924368d08f044c16c8de8ce6e692b5c
FranklinG1999 <franklinclara@outlook.com>
2019-11-12 09:35:33 -0600

    Adicione las variables en el Login y las defini a cada control

--- app/src/main/java/com/example/proyectocine/Login.java
+++ app/src/main/java/com/example/proyectocine/Login.java
@@ -3,12 +3,19 @@ package com.example.proyectocine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-String hola;
+    EditText et_usuario, et_pass;
+    Button btnlog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        et_usuario=(EditText)findViewById((R.id.et_usuario));
+        et_pass=(EditText)findViewById((R.id.et_pass));
+        btnlog=(Button) findViewById((R.id.btnlog));
     }
 }
