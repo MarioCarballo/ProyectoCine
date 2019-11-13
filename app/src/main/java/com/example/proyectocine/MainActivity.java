539f680d5497585b2fc747a4672a08519224790a
wilfredorivera01 <17wilfreriver@gmail.com>
2019-11-13 12:51:14 -0600

    elimine los bugss

--- app/src/main/java/com/example/proyectocine/MainActivity.java
+++ app/src/main/java/com/example/proyectocine/MainActivity.java
@@ -2,74 +2,39 @@ package com.example.proyectocine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
-    private WebView myWebView;
+
+
+    WebView browser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        this.myWebView = (WebView) this.findViewById(R.id.WebView);
+        browser=(WebView)findViewById(R.id.WebView);
 
-<<<<<<< HEAD
         WebSettings webSettings = browser.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
         browser.setWebViewClient(new WebViewClient(){
-=======
-        // Enable JavaScript WebSettings webSettings =
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
->>>>>>> 4864ff54af1a8c34db456c3a938184e45e7b53d1
 
-        // Provide a WebViewClient for your WebView
-        myWebView.setWebViewClient(new MyWebViewClient());
-
-        myWebView.loadUrl("http://mjgl.com.sv/cine/");
-
-    }
-
-    @Override
-    public void onBackPressed() {
-
-        // Check if there's history
-        if (this.myWebView.canGoBack())
-            this.myWebView.goBack();
-        else
-            super.onBackPressed();
-
-    }
-
-    private class MyWebViewClient extends WebViewClient {
-
-        private long loadTime; // Web page loading time
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-
-            if (Uri.parse(url).getHost().equals("Mi pagina web")) {
-                // This is my web site, so do not override; let my WebView load
-                // the page
-                return false;
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
             }
 
-            // Otherwise, the link is not for a page on my site, so launch
-            // another Activity that handles URLs
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            startActivity(intent);
-            return true;
+        });
+        // Cargamos la WEB
+        browser.loadUrl("http://mjgl.com.sv/cine/");
 
-        }
+
     }
-
 }
