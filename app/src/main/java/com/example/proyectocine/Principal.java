5d6eed983d3045258d401cb90069104285b5e6e2
galeano9911 <sofygaleano21@gmail.com>
2019-11-14 11:11:24 -0600

    cree una ueva activity

--- app/src/main/java/com/example/proyectocine/Principal.java
+++ app/src/main/java/com/example/proyectocine/Principal.java
@@ -2,13 +2,35 @@ package com.example.proyectocine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
 
 public class Principal extends AppCompatActivity {
-
+    Button btnusuario;
+    Button btncliente;
+    Button btnregistro;
+    Button btnrecuoperar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        btnusuario=(Button) findViewById((R.id.btnusuario));
+        btncliente=(Button) findViewById((R.id.btncliente));
+        btnregistro=(Button) findViewById((R.id.btnregistro));
+        btnrecuoperar=(Button) findViewById((R.id.btnrecuoperar));
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        btnusuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Principal.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
