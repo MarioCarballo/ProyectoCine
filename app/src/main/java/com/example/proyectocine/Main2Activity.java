26f66c7a6b5a1a6681d7f0bd9b3c907cfa77ce84
wilfredorivera01 <17wilfreriver@gmail.com>
2019-11-14 11:19:30 -0600

    anexe la url para la nueva vista del nuevo webview

--- app/src/main/java/com/example/proyectocine/Main2Activity.java
+++ app/src/main/java/com/example/proyectocine/Main2Activity.java
@@ -3,12 +3,34 @@ package com.example.proyectocine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Main2Activity extends AppCompatActivity {
 
+    WebView browser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        browser=(WebView)findViewById(R.id.WebView);
+
+        WebSettings webSettings = browser.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        browser.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+        });
+        // Cargamos la WEB
+        browser.loadUrl("https://mjgl.com.sv/cine/index.php?c=Login&a=Index");
+
+
     }
 }
