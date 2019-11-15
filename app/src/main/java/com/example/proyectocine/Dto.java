4e2caec85205c97813d703dc33c3a6d5a0d2bb90
wilfredorivera01 <17wilfreriver@gmail.com>
2019-11-15 09:53:49 -0600

    Cree la clase Dto y cree los parametros a instanciar

---
+++ app/src/main/java/com/example/proyectocine/Dto.java
@@ -0,0 +1,64 @@
+package com.example.proyectocine;
+
+import java.io.Serializable;
+
+public class Dto implements Serializable {
+    public String getNombre() {
+        return nombre;
+    }
+
+public Dto(){
+
+}
+
+//public Dto(int identificacion, String nombre, apellido, correo, contra1, contra2 ){
+
+//}
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContra1() {
+        return contra1;
+    }
+
+    public void setContra1(String contra1) {
+        this.contra1 = contra1;
+    }
+
+    public String getContra2() {
+        return contra2;
+    }
+
+    public void setContra2(String contra2) {
+        this.contra2 = contra2;
+    }
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    String nombre, apellido, correo, contra1, contra2;
+int identificacion;
+}
--- app/src/main/java/com/example/proyectocine/Login.java
+++ app/src/main/java/com/example/proyectocine/Login.java
@@ -11,33 +11,16 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-    EditText et_nombre, et_pass;
+    EditText et_nombre, et_apellido,et_identificacion,et_email,et_pass,etpas2;
     Button btnlog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        et_nombre=(EditText)findViewById((R.id.et_nombre));
-        et_pass=(EditText)findViewById((R.id.et_pass));
-        btnlog=(Button) findViewById((R.id.btnlog));
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        btnlog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String usu = et_nombre.getText().toString();
-                String pass = et_pass.getText().toString();
 
-                if (usu.equals("Admin")&& pass.equals("123")){
-                    Intent intent = new Intent(Login.this, MainActivity.class);
-                    startActivity(intent);
-                }else{
-                    et_nombre.setText(null);
-                    et_pass.setText(null);
-                    Toast.makeText(Login.this, "Usuario o contraseña incorrectas", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+
+
     }
 }
