b44da463047b67c75e6512734204ed02f2272ddc
FranklinG1999 <franklinclara@outlook.com>
2019-11-15 10:06:54 -0600

    Agregue la clase usuario

---
+++ app/src/main/java/com/example/proyectocine/Usuario.java
@@ -0,0 +1,53 @@
+package com.example.proyectocine;
+
+public class Usuario {
+    String nombre;
+    String apellido;
+    String email;
+    int clave;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellido, String email, int clave) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.clave = clave;
+
+    }
+
+    public String getNombre() { return nombre; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+    public int getClave() {
+        return clave;
+    }
+
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+}
