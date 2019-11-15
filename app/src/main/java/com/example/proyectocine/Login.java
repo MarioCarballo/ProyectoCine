63c95d04d17c5b5fe4d3f8ca529158a18c24d911
wilfredorivera01 <17wilfreriver@gmail.com>
2019-11-15 10:28:45 -0600

    E iniciado la funcion consultar por codigo

--- app/src/main/java/com/example/proyectocine/Login.java
+++ app/src/main/java/com/example/proyectocine/Login.java
@@ -1,8 +1,14 @@
 package com.example.proyectocine;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -13,14 +19,28 @@ import android.widget.Toast;
 public class Login extends AppCompatActivity {
     EditText et_nombre, et_apellido,et_identificacion,et_email,et_pass,etpas2;
     Button btnlog;
+
+    Dto datos = new Dto();
+
+    private ProgressDialog pd;
+    AlertDialog.Builder dialogo1;
+    AlertDialog.Builder dialogo;
+    ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
-
-
-
     }
+
+    public void consultarCodigo(final Context context, final String codigo) {
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Espere por favor, Estamos trabajando en su petición en el servidor");
+        progressDialog.show();
+
+        String url = Config.urlConsultaCodigo;
+    }
+
 }
