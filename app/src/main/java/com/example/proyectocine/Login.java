eec5ae856ad6b7ff21147365cf51f57c8f9ce239
wilfredorivera01 <17wilfreriver@gmail.com>
2019-11-15 11:12:48 -0600

    Elimine una activity y cre dos mas para crear registros de empleados

--- app/src/main/java/com/example/proyectocine/Login.java
+++ app/src/main/java/com/example/proyectocine/Login.java
@@ -1,14 +1,8 @@
 package com.example.proyectocine;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -17,30 +11,33 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-    EditText et_nombre, et_apellido,et_identificacion,et_email,et_pass,etpas2;
+    EditText et_nombre, et_pass;
     Button btnlog;
-
-    Dto datos = new Dto();
-
-    private ProgressDialog pd;
-    AlertDialog.Builder dialogo1;
-    AlertDialog.Builder dialogo;
-    ProgressDialog progressDialog;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        et_nombre=(EditText)findViewById((R.id.et_nombre));
+        et_pass=(EditText)findViewById((R.id.et_pass));
+        btnlog=(Button) findViewById((R.id.btnlog));
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        btnlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usu = et_nombre.getText().toString();
+                String pass = et_pass.getText().toString();
+
+                if (usu.equals("Admin")&& pass.equals("123")){
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    et_nombre.setText(null);
+                    et_pass.setText(null);
+                    Toast.makeText(Login.this, "Usuario o contraseña incorrectas", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
-
-    public void consultarCodigo(final Context context, final String codigo) {
-        progressDialog = new ProgressDialog(context);
-        progressDialog.setCancelable(false);
-        progressDialog.setMessage("Espere por favor, Estamos trabajando en su petición en el servidor");
-        progressDialog.show();
-
-        String url = Config.urlConsultaCodigo;
-    }
-
 }
