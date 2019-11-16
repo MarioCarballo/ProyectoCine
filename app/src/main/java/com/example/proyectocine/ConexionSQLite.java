86a46f7a1eba669ac916feaca1f8c8a7fcdedb9c
wilfredorivera01 <17wilfreriver@gmail.com>
2019-11-16 10:52:58 -0600

    cree la base de datos con su tabla usuarios

--- app/src/main/java/com/example/proyectocine/ConexionSQLite.java
+++ app/src/main/java/com/example/proyectocine/ConexionSQLite.java
@@ -1,10 +1,35 @@
 package com.example.proyectocine;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 import java.util.ArrayList;
 
-public class ConexionSQLite {
-
-    ArrayList<String> listaEmpleado;
-    ArrayList<Dto> empleadolist;
+public class ConexionSQLite extends SQLiteOpenHelper {
+    ArrayList<String> listausuarios;
+    ArrayList<Dto> usuarioslist;
     boolean estadoDelete = true;
+
+
+    public ConexionSQLite(Context context) {
+        super(context, "administracion.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table usuarios (codigo integer not null primary key, nombre text, apellido tex,correo text, contraseña text)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("drop table if exists usuarios");
+        onCreate(db);
+    }
+
+    public SQLiteDatabase bd() {
+        SQLiteDatabase bd = this.getWritableDatabase();
+        return bd;
+    }
 }
