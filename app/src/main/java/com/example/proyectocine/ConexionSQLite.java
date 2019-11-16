398210681304e9c665ee2adabada7db5d2746a79
wilfredorivera01 <17wilfreriver@gmail.com>
2019-11-16 10:36:16 -0600

    cree una nueva clase de conexion a la base de datos local

---
+++ app/src/main/java/com/example/proyectocine/ConexionSQLite.java
@@ -0,0 +1,10 @@
+package com.example.proyectocine;
+
+import java.util.ArrayList;
+
+public class ConexionSQLite {
+
+    ArrayList<String> listaEmpleado;
+    ArrayList<Dto> empleadolist;
+    boolean estadoDelete = true;
+}
