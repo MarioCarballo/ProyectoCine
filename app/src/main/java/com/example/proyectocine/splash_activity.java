d737c7b8acb5ddb6594f959bc4c3818ff2be6331
FranklinG1999 <franklinclara@outlook.com>
2019-11-17 18:01:48 -0600

    Adicione los codigos para el Splash

--- app/src/main/java/com/example/proyectocine/splash_activity.java
+++ app/src/main/java/com/example/proyectocine/splash_activity.java
@@ -2,13 +2,30 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
-public class splash_activity extends AppCompatActivity {
-
+ public class splash_activity extends AppCompatActivity {
+private final int DURACION_SPLASH =2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_activity);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent= new Intent(splash_activity.this, Principal.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+
     }
 }
